b6d042a04c3c6b058bb36d3ae2acfcb087a5ebac
pashka <hvalyginpavel@gmail.com>
2022-11-28 13:41:49 +0200

    - Refactor migration - Add OrderList script

--- migrations/Version20221127154004.php
+++ migrations/Version20221127154004.php
@@ -25,51 +25,13 @@ final class Version20221127154004 extends AbstractMigration
             'instances'
         ])) {
             $this->addSql('CREATE TABLE clients (id BIGINT UNSIGNED AUTO_INCREMENT NOT NULL, created_at DATETIME DEFAULT NULL, updated_at DATETIME DEFAULT NULL, deleted_at DATETIME DEFAULT NULL, server_id BIGINT UNSIGNED DEFAULT NULL, service LONGTEXT CHARACTER SET utf8mb4 DEFAULT NULL COLLATE `utf8mb4_0900_ai_ci`, date DATETIME DEFAULT NULL, is_active TINYINT(1) DEFAULT NULL, config_file_name LONGTEXT CHARACTER SET utf8mb4 DEFAULT NULL COLLATE `utf8mb4_0900_ai_ci`, INDEX idx_clients_deleted_at (deleted_at), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_0900_ai_ci` ENGINE = InnoDB COMMENT = \'\' ');
-            $this->abortIf(
-                !$this->connection->getDatabasePlatform() instanceof \Doctrine\DBAL\Platforms\MySQL80Platform,
-                "Migration can only be executed safely on '\Doctrine\DBAL\Platforms\MySQL80Platform'."
-            );
-
             $this->addSql('CREATE TABLE instances (id INT AUTO_INCREMENT NOT NULL, created_at DATETIME DEFAULT NULL, updated_at DATETIME DEFAULT NULL, deleted_at DATETIME DEFAULT NULL, ip VARCHAR(191) CHARACTER SET utf8mb4 DEFAULT NULL COLLATE `utf8mb4_0900_ai_ci`, is_active TINYINT(1) NOT NULL, country LONGTEXT CHARACTER SET utf8mb4 DEFAULT NULL COLLATE `utf8mb4_0900_ai_ci`, region LONGTEXT CHARACTER SET utf8mb4 DEFAULT NULL COLLATE `utf8mb4_0900_ai_ci`, city LONGTEXT CHARACTER SET utf8mb4 DEFAULT NULL COLLATE `utf8mb4_0900_ai_ci`, http_url LONGTEXT CHARACTER SET utf8mb4 DEFAULT NULL COLLATE `utf8mb4_0900_ai_ci`, version LONGTEXT CHARACTER SET utf8mb4 DEFAULT NULL COLLATE `utf8mb4_0900_ai_ci`, up_date DATETIME DEFAULT NULL, secret LONGTEXT CHARACTER SET utf8mb4 DEFAULT NULL COLLATE `utf8mb4_0900_ai_ci`, available_services LONGTEXT CHARACTER SET utf8mb4 DEFAULT NULL COLLATE `utf8mb4_0900_ai_ci`, UNIQUE INDEX ip (ip), INDEX idx_instances_deleted_at (deleted_at), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_0900_ai_ci` ENGINE = InnoDB COMMENT = \'\' ');
         }
-
-        $this->abortIf(
-            !$this->connection->getDatabasePlatform() instanceof \Doctrine\DBAL\Platforms\MySQL80Platform,
-            "Migration can only be executed safely on '\Doctrine\DBAL\Platforms\MySQL80Platform'."
-        );
-
         $this->addSql('CREATE TABLE payment (id INT AUTO_INCREMENT NOT NULL, order_id INT DEFAULT NULL, external_id VARCHAR(255) CHARACTER SET utf8mb4 DEFAULT NULL COLLATE `utf8mb4_unicode_ci`, type VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, date DATE NOT NULL, status VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, is_initial TINYINT(1) DEFAULT 0 NOT NULL, price DOUBLE PRECISION NOT NULL, currency VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, created_at DATETIME NOT NULL, updated_at DATETIME NOT NULL, deleted_at DATETIME DEFAULT NULL, INDEX IDX_6D28840D8D9F6D38 (order_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB COMMENT = \'\' ');
-        $this->abortIf(
-            !$this->connection->getDatabasePlatform() instanceof \Doctrine\DBAL\Platforms\MySQL80Platform,
-            "Migration can only be executed safely on '\Doctrine\DBAL\Platforms\MySQL80Platform'."
-        );
-
-        $this->addSql('CREATE TABLE `order` (id INT AUTO_INCREMENT NOT NULL, user_id INT DEFAULT NULL, server_id INT DEFAULT NULL, is_active TINYINT(1) DEFAULT 0 NOT NULL, device_count INT NOT NULL, start_date DATE NOT NULL, end_date DATE NOT NULL, created_at DATETIME NOT NULL, updated_at DATETIME NOT NULL, deleted_at DATETIME DEFAULT NULL, INDEX IDX_F5299398A76ED395 (user_id), INDEX IDX_F52993981844E6B7 (server_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB COMMENT = \'\' ');
-        $this->abortIf(
-            !$this->connection->getDatabasePlatform() instanceof \Doctrine\DBAL\Platforms\MySQL80Platform,
-            "Migration can only be executed safely on '\Doctrine\DBAL\Platforms\MySQL80Platform'."
-        );
-
+        $this->addSql('CREATE TABLE `order` (id INT AUTO_INCREMENT NOT NULL, user_id INT DEFAULT NULL, server_id INT DEFAULT NULL, is_active TINYINT(1) DEFAULT 0 NOT NULL, start_date DATE NOT NULL, end_date DATE NOT NULL, created_at DATETIME NOT NULL, updated_at DATETIME NOT NULL, deleted_at DATETIME DEFAULT NULL, INDEX IDX_F5299398A76ED395 (user_id), INDEX IDX_F52993981844E6B7 (server_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB COMMENT = \'\' ');
         $this->addSql('CREATE TABLE plan (id INT AUTO_INCREMENT NOT NULL, is_active TINYINT(1) NOT NULL, days_count INT NOT NULL, price DOUBLE PRECISION NOT NULL, minimum_device_count INT DEFAULT NULL, created_at DATETIME NOT NULL, updated_at DATETIME NOT NULL, deleted_at DATETIME DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB COMMENT = \'\' ');
-        $this->abortIf(
-            !$this->connection->getDatabasePlatform() instanceof \Doctrine\DBAL\Platforms\MySQL80Platform,
-            "Migration can only be executed safely on '\Doctrine\DBAL\Platforms\MySQL80Platform'."
-        );
-
         $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, chat_id INT NOT NULL, created_at DATETIME NOT NULL, updated_at DATETIME NOT NULL, deleted_at DATETIME DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB COMMENT = \'\' ');
-        $this->abortIf(
-            !$this->connection->getDatabasePlatform() instanceof \Doctrine\DBAL\Platforms\MySQL80Platform,
-            "Migration can only be executed safely on '\Doctrine\DBAL\Platforms\MySQL80Platform'."
-        );
-
         $this->addSql('ALTER TABLE user ADD username VARCHAR(255) DEFAULT NULL;');
-
-        $this->addSql('CREATE TABLE device (id INT AUTO_INCREMENT NOT NULL, order_id INT DEFAULT NULL, unique_hash VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, ip VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, created_at DATETIME NOT NULL, updated_at DATETIME NOT NULL, deleted_at DATETIME DEFAULT NULL, INDEX IDX_92FB68E8D9F6D38 (order_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB COMMENT = \'\' ');
-        $this->abortIf(
-            !$this->connection->getDatabasePlatform() instanceof \Doctrine\DBAL\Platforms\MySQL80Platform,
-            "Migration can only be executed safely on '\Doctrine\DBAL\Platforms\MySQL80Platform'."
-        );
-
         $this->addSql('ALTER TABLE clients ADD user_id INT NULL;');
         $this->addSql('ALTER TABLE clients ADD CONSTRAINT FK_C82E74A76ED395 FOREIGN KEY (user_id) REFERENCES user (id);');
         $this->addSql('CREATE INDEX IDX_C82E74A76ED395 ON clients (user_id);');
--- src/Domain/Scripts/OrderList.php
+++ src/Domain/Scripts/OrderList.php
@@ -3,7 +3,9 @@
 namespace App\Domain\Scripts;
 
 use App\Domain\AbstractScript;
+use App\Domain\Entity\Message;
 use App\Domain\Entity\Query;
+use App\Entity\Order;
 use App\Kernel;
 use App\Service\Telegram\TelegramScript;
 use Doctrine\ORM\EntityManagerInterface;
@@ -25,6 +27,64 @@ class OrderList extends AbstractScript
 
     public function handle(Query $query): void
     {
-        // TODO: Implement handle() method.
+        /** @var Order[] $activeOrders */
+        $activeOrders = $this->em->getRepository(Order::class)->findBy([
+            'isActive' => true
+        ]);
+
+        /** @var Order[] $inactiveOrders */
+        $inactiveOrders = $this->em->getRepository(Order::class)->findBy([
+            'isActive' => false
+        ]);
+
+        if (empty($activeOrders) && empty($inactiveOrders)) {
+            $this->send(
+                new Message($query->chatId, 'Not order found :-(')
+            );
+            return;
+        }
+
+        if (!empty($activeOrders)) {
+            $this->send(
+                new Message($query->chatId, 'Active orders:')
+            );
+
+            foreach ($activeOrders as $order) {
+                $this->send(
+                    new Message($query->chatId, $this->formatOrder($order))
+                );
+            }
+        }
+
+        if (!empty($inactiveOrders)) {
+            $this->send(
+                new Message($query->chatId, 'Active orders:')
+            );
+
+            foreach ($inactiveOrders as $order) {
+                $this->send(
+                    new Message($query->chatId, $this->formatOrder($order))
+                );
+            }
+        }
+    }
+
+    private const FORMAT_ORDER = 'Order #%s
+    User: %s
+    Instance id: %s
+    Start date: %s
+    End date: %s
+    ';
+
+    private function formatOrder(Order $order): string
+    {
+        return sprintf(
+            self::FORMAT_ORDER,
+            $order->id,
+            $order->user->username,
+            $order->server->id,
+            $order->startDate->format('Y-m-d'),
+            $order->endDate->format('Y-m-d')
+        );
     }
 }
